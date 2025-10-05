93e0e8e9d0ddd876656e49f292a4a2b92caec9b1
ethio py <ethio@gmail.com>
2025-10-05 14:58:03 +0300

    Implement core Event and Booking API endpoints

--- events_booking/events_booking/urls.py
+++ events_booking/events_booking/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from events.views import EventViewSet
+from bookings.views import BookingViewSet
+
+router = routers.DefaultRouter()
+router.register(r'events', EventViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
