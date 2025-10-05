eb9f09e8621f384dfab63bc315ef5319d699d8b8
ethio py <ethio@gmail.com>
2025-10-05 14:40:15 +0300

    Initial commit - setup project and add .gitignore

---
+++ events_booking/events/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    capacity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
