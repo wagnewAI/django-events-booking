a1b15808d0dc8f493429d5a0fd57525868e78703
Wagnew Moges <wagnewm143@gmail.com>
2025-10-12 18:21:35 +0300

    Implement booking cancellation logic

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
