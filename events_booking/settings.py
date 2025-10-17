bdf25a04520b5facf07a0a4f147c61ef8613bbd3
wagnewAI <wagnewm143@gmail.com>
2025-10-17 09:52:21 +0300

    Enhanced Event model, added timestamps, organizer, and DRF setup

--- events_booking/settings.py
+++ events_booking/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ht=eirzbj*&y4_$o4%0m7ip++yre#w^4b=kf5t7k9c+e!7az$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
