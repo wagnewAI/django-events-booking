1b91416713db5d19ed3f8809aceed839f1304935
wagnewAI <wagnewm143@gmail.com>
2025-10-18 17:33:49 +0300

    user

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth.models import User
+# Create your models here
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from .serializers import UserSerializer
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
